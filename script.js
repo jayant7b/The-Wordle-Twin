d89157c44c6ba39ea66606f62d0db4eb07b337de
Jayant B <50587969+jayant7b@users.noreply.github.com>
2022-06-12 17:47:16 +0530

    Grid input implementation
    Implementation of
    - Input
    - Backspace
    - Enter

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+// grid size
 var height = 6;
 var width = 5;
 
+// cursor index
 var row = 0, col = 0;
+
+var gameOver = false;
 
 window.onload = function(){
     intialize();
 }
 
 function intialize(){
-    //create game board
+    // create game board
     for (let r = 0; r < height; r++) {
         for (let c = 0; c < width; c++) {
-            //<span id = "0_0" class = "tile"> </span>
+            // creates: <span id = "0_0" class = "tile"> </span>
             let tile = document.createElement("span");
             tile.id = r.toString() + "_" + c.toString();
             tile.classList.add("tile");
@@ -20,3 +24,77 @@ function intialize(){
         }
     }
 }
+
+// checks if wordList contains word; gets input word as paramter
+function isValidWord(){
+    // TODO: checks if wordList contains word; gets input word as paramter
+    return true;
+}
+
+// process word to display color coding
+function update(){
+    // TODO: process word to display color coding; get input word as parameter
+}
+
+// TODO snippet: to use later
+function getWord(){
+    var word = "";
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + "_" + c.toString());
+        word += currTile.innerText.toString();
+    }
+    return word;
+}
+
+// keydown makes 'aaaaa' input possible
+document.addEventListener("keyup", (ev) => {
+    if(gameOver){
+        return;
+    }
+
+    //TODO: skip for ctrl+r; ctrl+z; other combinations
+    //alert(ev.code);
+
+    // A-Z, Enter, Del allowed
+    // user input A-Z
+    if(ev.code >= "KeyA" && ev.code <= "KeyZ"){
+
+        // input
+        if(col < width){
+            // if current tile is blank, input
+            let currTile = document.getElementById(row.toString() + "_" + col.toString());
+            if(currTile.innerText == ""){
+                // 'A' of "keyA"
+                currTile.innerText = ev.code[3];
+                currTile.classList.add("entered");
+                col++;
+            }
+        }
+    }
+    // Del last input letter
+    else if(ev.code == "Backspace"){
+        if(col <= width && col > 0){
+            col--;
+            let currTile = document.getElementById(row.toString() + "_" + col.toString());
+            currTile.innerText = "";
+            currTile.classList.replace("entered", "blank");
+        }
+    }
+    // submit answer; goto next column
+    else if(ev.code == "Enter"){
+        if((col == width) && isValidWord()){
+            update();
+            row++;
+            col = 0;
+        }
+        else{
+            // TODO: proper alert popup
+            alert("Not enough letters");
+        }
+    }
+
+    // six failed attempts
+    if(!gameOver && row == height){
+        gameOver == true;
+    }
+})
